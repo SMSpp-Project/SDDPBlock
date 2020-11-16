0d35fdff61125cba26adea8f30bea485f50e5298
Niccolò Iardella <niccolo.iardella@for.unipi.it>
2020-11-16 20:03:03 +0100

    Add a lot of revisions and comments to CMake files

--- cmake/FindStOpt.cmake
+++ cmake/FindStOpt.cmake
@@ -1,15 +1,37 @@
 # --------------------------------------------------------------------------- #
 #    CMake find module for StOpt                                              #
 #                                                                             #
-#    Accepts the following PATHS:                                             #
+#    This module finds StOpt include directories and libraries.               #
+#    Use it by invoking find_package() with the form:                         #
 #                                                                             #
-#    - STOPT_INC - Custom path to StOpt headers                               #
-#    - STOPT_LIB - Custom path to StOpt libraries                             #
+#        find_package(StOpt [version] [EXACT] [REQUIRED])                     #
 #                                                                             #
-#    Provides the following imported targets:                                 #
+#    The results are stored in the following variables:                       #
 #                                                                             #
-#    - StOpt::StOpt - the StOpt library                                       #
-#    - StOpt::geners - the geners library                                     #
+#        <LibraryName>_FOUND         - True if headers are found              #
+#        <LibraryName>_INCLUDE_DIRS  - Include directories                    #
+#        <LibraryName>_LIBRARIES     - Libraries to be linked                 #
+#        <LibraryName>_VERSION       - Version number                         #
+#                                                                             #
+#    The search results are saved in these persistent cache entries:          #
+#                                                                             #
+#        <LibraryName>_INCLUDE_DIR   - Directory containing headers           #
+#        <LibraryName>_LIBRARY       - The found library                      #
+#                                                                             #
+#    for each <LibraryName> in: StOpt, StOpt_geners.                          #
+#                                                                             #
+#    This module reads hints about search locations from variables:           #
+#                                                                             #
+#        STOPT_INC                   - Preferred include directory            #
+#        STOPT_LIB                   - Preferred library directory            #
+#                                                                             #
+#    The following IMPORTED targets are also defined:                         #
+#                                                                             #
+#        StOpt::StOpt                                                         #
+#        StOpt::geners                                                        #
+#                                                                             #
+#    This find module is provided because StOpt does not provide              #
+#    a CMake configuration file on its own.                                   #
 #                                                                             #
 #                              Niccolo' Iardella                              #
 #                          Operations Research Group                          #
@@ -25,64 +47,66 @@ find_package(Boost REQUIRED COMPONENTS system timer QUIET)
 
 # This will try first with Eigen3 own configuration file,
 # then with the find module we provide.
-find_package(Eigen3 QUIET NO_MODULE)
+find_package(Eigen3 QUIET CONFIG)
 if (NOT Eigen3_FOUND)
-    get_filename_component(FIND_MODULE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
-    list(APPEND CMAKE_MODULE_PATH ${FIND_MODULE_DIR})
     find_package(Eigen3 REQUIRED)
-    list(REMOVE_AT CMAKE_MODULE_PATH -1)
 endif ()
 
 # ----- Find the geners library --------------------------------------------- #
-find_path(geners_INCLUDE_DIR
+# Note that find_path() creates a cache entry
+find_path(StOpt_geners_INCLUDE_DIR
           NAMES geners/uriUtils.hh
           PATHS ${STOPT_INC}
-          DOC "geners include directory")
+          DOC "geners include directory.")
 
-find_library(geners_LIBRARY
+# Note that find_library() creates a cache entry
+find_library(StOpt_geners_LIBRARY
              NAMES geners
              PATHS ${STOPT_LIB}
-             DOC "geners library")
-
-mark_as_advanced(geners_INCLUDE_DIR geners_LIBRARY)
+             DOC "geners library.")
 
 # ----- Find the StOpt library ---------------------------------------------- #
+# Note that find_path() creates a cache entry
 find_path(StOpt_INCLUDE_DIR
           NAMES StOpt/sddp/OptimizerSDDPBase.h
           PATHS ${STOPT_INC}
-          DOC "StOpt include directory")
+          DOC "StOpt include directory.")
 
+# Note that find_library() creates a cache entry
 find_library(StOpt_LIBRARY
              NAMES StOpt
              PATHS ${STOPT_LIB}
-             DOC "StOpt library")
-
-mark_as_advanced(StOpt_INCLUDE_DIR StOpt_LIBRARY)
+             DOC "StOpt library.")
 
 # ----- Handle the standard arguments --------------------------------------- #
+# The following macro manages the QUIET, REQUIRED and version-related options
+# passed to find_package(). It also sets <PackageName>_FOUND if REQUIRED_VARS
+# are set. REQUIRED_VARS should be cache entries and not output variables.
+# See:
+# https://cmake.org/cmake/help/latest/module/FindPackageHandleStandardArgs.html
 find_package_handle_standard_args(
         StOpt
         REQUIRED_VARS
-        geners_LIBRARY geners_INCLUDE_DIR
+        StOpt_geners_INCLUDE_DIR StOpt_geners_LIBRARY
         StOpt_LIBRARY StOpt_INCLUDE_DIR)
 
-# ----- Export the target(s) ------------------------------------------------ #
+# ----- Export the targets -------------------------------------------------- #
 if (StOpt_FOUND)
-    if (NOT TARGET StOpt::geners)
-        set(geners_INCLUDE_DIRS "${geners_INCLUDE_DIR}")
-        set(geners_LIBRARIES "${geners_LIBRARY}")
+    set(StOpt_geners_INCLUDE_DIRS "${StOpt_geners_INCLUDE_DIR}")
+    set(StOpt_geners_LIBRARIES "${StOpt_geners_LIBRARY}")
 
+    if (NOT TARGET StOpt::geners)
         add_library(StOpt::geners UNKNOWN IMPORTED)
         set_target_properties(
                 StOpt::geners PROPERTIES
-                IMPORTED_LOCATION "${geners_LIBRARY}"
-                INTERFACE_INCLUDE_DIRECTORIES "${geners_INCLUDE_DIR}")
+                IMPORTED_LOCATION "${StOpt_geners_LIBRARY}"
+                INTERFACE_INCLUDE_DIRECTORIES "${StOpt_geners_INCLUDE_DIR}")
     endif ()
 
-    if (NOT TARGET StOpt::StOpt)
-        set(StOpt_INCLUDE_DIRS "${StOpt_INCLUDE_DIR}")
-        set(StOpt_LIBRARIES "${StOpt_LIBRARY}")
+    set(StOpt_INCLUDE_DIRS "${StOpt_INCLUDE_DIR}")
+    set(StOpt_LIBRARIES "${StOpt_LIBRARY}")
 
+    if (NOT TARGET StOpt::StOpt)
         add_library(StOpt::StOpt UNKNOWN IMPORTED)
         set_target_properties(
                 StOpt::StOpt PROPERTIES
@@ -91,3 +115,10 @@ if (StOpt_FOUND)
                 INTERFACE_LINK_LIBRARIES "StOpt::geners;Eigen3::Eigen;BZip2::BZip2;ZLIB::ZLIB;Boost::system;Boost::timer")
     endif ()
 endif ()
+
+# Variables marked as advanced are not displayed in CMake GUIs, see:
+# https://cmake.org/cmake/help/latest/command/mark_as_advanced.html
+mark_as_advanced(StOpt_geners_INCLUDE_DIR
+                 StOpt_geners_LIBRARY
+                 StOpt_INCLUDE_DIR
+                 StOpt_LIBRARY)
